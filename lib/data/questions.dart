0842a8af963ddce06b3b63fc7ab92ebced80882c
John Daniel Atienza <mashiroyuzuhira12@gmail.com>
2026-09-06 12:01:04 +0800

    Add custom question bank data source

---
+++ lib/data/questions.dart
@@ -0,0 +1,40 @@
+import 'package:quiz_app/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What does SQL stand for in database management?',
+    [
+      'Structured Query Language',
+      'Simple Question Language',
+      'Structured Queueing List',
+      'System Query Logic',
+    ],
+  ),
+  QuizQuestion(
+    'Which version control system are we using for this project?',
+    [
+      'Git',
+      'Subversion',
+      'Mercurial',
+      'CVS',
+    ],
+  ),
+  QuizQuestion(
+    'Which widget is commonly used for repeating layouts in Flutter?',
+    [
+      'ListView',
+      'ContainerView',
+      'RepeatLayout',
+      'StackBox',
+    ],
+  ),
+  QuizQuestion(
+    'What programming language is used to build Flutter applications?',
+    [
+      'Dart',
+      'Kotlin',
+      'Swift',
+      'Java',
+    ],
+  ),
+];
